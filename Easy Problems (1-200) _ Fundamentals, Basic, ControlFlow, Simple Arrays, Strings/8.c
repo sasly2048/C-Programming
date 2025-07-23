382bacdc8bf328396d2abc72bfe8a102e781aad9
Raghavendra G <raghavendrasujith204800@gmail.com>
2025-07-23 20:05:01 +0530

    Add solution to problem 8 in easy C problems

---
+++ Easy Problems (1-200) _ Fundamentals, Basic, ControlFlow, Simple Arrays, Strings/8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+
+    printf("Enter Two Variables : ");
+    scanf("%f %f",&a,&b);
+
+    printf("The Product of variables is : %f",a*b);
+
+    return 0;
+}
